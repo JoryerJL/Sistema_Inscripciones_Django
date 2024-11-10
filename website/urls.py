5b6f7e01d896aafb6fa2add5bbd4055a338fb79d
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-09 21:17:29 -0600

    crear vista index y detalles

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ app_name = 'website'
 
 urlpatterns = [
     path('', index_website, name='index'),
+    path('detail_course/', detail_course, name='detail_course'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index_website(request):
     return render(request, 'website/index.html')
+
+def detail_course(request):
+    return render(request, 'website/detail_course.html')
