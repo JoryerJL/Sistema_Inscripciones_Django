b4a99e18642e42bf301035e79f29b86e5a04954c
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-17 12:04:35 -0600

    Crear modelo curso ofertado

--- cursos/models.py
+++ cursos/models.py
@@ -5,8 +5,6 @@ class Curso(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=50)
     descripcion = models.TextField(blank=True)
-    #precio = models.DecimalField(max_digits=8, decimal_places=2)
-    #|fecha_inicio = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.titulo
@@ -16,3 +14,24 @@ class Curso(models.Model):
         ordering = ['titulo']
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
+
+class CursoOfertado(models.Model):
+    sedes = (
+        (1, 'Plantel Centro'),
+        (2, 'Plantel Tenosique'),
+    )
+    instructor = models.CharField(max_length=250)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    fecha_inicio = models.DateField(blank=True, null=True)
+    sede = models.PositiveIntegerField(choices=sedes)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    capacidad = models.PositiveSmallIntegerField(default=15)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.curso.titulo} - {self.fecha_inicio}'
+
+    class Meta:
+        db_table = 'curso_ofertado'
+        verbose_name = 'Curso Ofertado'
+        verbose_name_plural = 'Cursos Ofertados'
