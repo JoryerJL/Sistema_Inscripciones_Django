df960eef3dd6b81766345527d2b08f94ca62404d
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-17 10:27:25 -0600

    agregado vista dashboard curso

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import *
 
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/index.html'), name='dashboard'),
+    path('lista-curso/', lista_curso, name='lista_curso'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_curso(request):
+    return render(request, 'dashboard/cursos/lista_curso.html')
