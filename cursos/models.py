b369b5f52a37d7d97e3c9a9737a7429d734683cb
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-20 23:32:54 -0600

    Agregando modelos para preinscripcion

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -42,7 +43,7 @@ class CursoOfertado(models.Model):
     estatus = models.ForeignKey(EstatusCursoOfertado , on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.curso.titulo} - {self.fecha_inicio}'
+        return f'{self.curso.titulo} - {self.get_sede_display()} - {self.estatus}'
 
     class Meta:
         db_table = 'curso_ofertado'
@@ -59,3 +60,36 @@ class CategoriaCurso (models.Model) :
 
     class Meta:
         db_table = 'categoria_curso'
+
+class EstatusPreinscripcion(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'estatus_preinscripcion_curso'
+        verbose_name = 'Estatus Preinscripcion Curso'
+        verbose_name_plural = 'Estatus Preinscripciones Cursos'
+
+    def __str__(self):
+        return self.nombre
+
+class PreinscripcionCurso (models.Model) :
+    curso = models.ForeignKey(CursoOfertado, on_delete=models.CASCADE)
+    prospecto = models.ForeignKey(User, on_delete=models.CASCADE)
+    comprobante_domicilio = models.FileField(upload_to='archivos_incripcion')
+    curp = models.FileField(upload_to='archivos_incripcion')
+    identificacion_oficial = models.FileField(upload_to='archivos_incripcion')
+    comprobante_pago = models.FileField(upload_to='archivos_incripcion',  blank=True, null=True)
+    comprobante_estudios = models.FileField(upload_to='archivos_incripcion')
+    acta_nacimiento = models.FileField(upload_to='archivos_incripcion')
+    estatus = models.ForeignKey(EstatusPreinscripcion, on_delete=models.CASCADE)
+    nota = models.CharField(max_length=250, blank=True)
+    fecha_solicitud = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'preinscripcion_curso'
+        verbose_name = 'Preinscripcion'
+        verbose_name_plural = 'Preinscripciones'
+
+    def __str__(self):
+        return f'{self.prospecto.first_name} - {self.curso.curso.titulo}'
