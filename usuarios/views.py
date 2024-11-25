b13cbfca67d4aa749f6907a09c63a337ff3d84ac
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-24 18:29:36 -0600

    update profile data form working

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,28 @@
 from lib2to3.fixes.fix_input import context
+from pyexpat.errors import messages
 
 from django.shortcuts import render
 from cursos.models import *
+from .forms import *
 
 # Create your views here.
 def perfil_alumno(request):
+    messageSuccess = ''
+    messageError = ''
+    form = UserModelForm(instance=request.user)
+    if request.method == 'POST':
+        form = UserModelForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messageSuccess = 'Datos actualizados'
+        else:
+            messageError = form.errors
     cursos = PreinscripcionCurso.objects.filter(prospecto=request.user)
     context = {
-        'cursos': cursos
+        'cursos': cursos,
+        'form': form,
+        'messageSuccess': messageSuccess,
+        'messageError': messageError
+
     }
     return render(request, 'usuarios/perfil_alumno.html', context)
